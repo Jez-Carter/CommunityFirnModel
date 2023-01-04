a1869a7181e475ec7169a8409cb58dd851298204
Jez-Carter <j.carter10@lancaster.ac.uk>
2023-01-04 21:02:26 +0000

    Including some processed data

---
+++ CFM_Lancaster/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    packages=find_packages(),
+)
+
+# setup(
+#     # ...
+#     package_dir = {"": "src"}
+#     # directory containing all the packages (e.g.  src/mypkg, src/mypkg/subpkg1, ...)
+# )
+
+# #python print folder of current 
